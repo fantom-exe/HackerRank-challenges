a52ca5f5f394100f630c8800af565dac6dde6898
Edward <fantom-exe@tuta.io>
2022-09-25 17:51:54 -0700

    done

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,6 +1,16 @@
 
 def fizzBuzz(n):
+    for i in range(1, n+1):
+        output = ''
+        if i % 3 == 0:
+            output += 'Fizz'
+        if i % 5 == 0:
+            output += 'Buzz'
 
+        if output == '':
+            output = i
+
+        print(output)
 
 n = 16
 print(fizzBuzz(n))
